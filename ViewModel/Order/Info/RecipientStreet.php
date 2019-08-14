c3a5ce7bb6b71d438e5e5fbe8e88cc53fe85fe3a
Sebastian Ertner <sebastian.ertner@netresearch.de>
2019-08-14 10:50:31 +0200

    DHLGW-469: update interface refs

--- ViewModel/Order/Info/RecipientStreet.php
+++ ViewModel/Order/Info/RecipientStreet.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 
 namespace Dhl\Ui\ViewModel\Order\Info;
 
-use Dhl\ShippingCore\Api\RecipientStreetInterface;
+use Dhl\ShippingCore\Api\Data\RecipientStreetInterface;
 use Magento\Framework\AuthorizationInterface;
 use Magento\Framework\Escaper;
 use Magento\Framework\UrlInterface;
