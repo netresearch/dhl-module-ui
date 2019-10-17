77928cba16a8466e96f396c0a6221221d4427a23
Paul Siedler <paul.siedler@netresearch.de>
2019-10-17 15:54:39 +0200

    DHLGW-480: refactor services view model on order to use unified way of retrieving services

--- ViewModel/Order/Info/ShippingServices.php
+++ ViewModel/Order/Info/ShippingServices.php
@@ -10,14 +10,12 @@ use Dhl\ShippingCore\Api\Data\ShippingOption\InputInterface;
 use Dhl\ShippingCore\Api\Data\ShippingOption\OptionInterface;
 use Dhl\ShippingCore\Api\Data\ShippingOption\Selection\AssignedSelectionInterface;
 use Dhl\ShippingCore\Api\Data\ShippingOption\Selection\SelectionInterface;
-use Dhl\ShippingCore\Model\Packaging\PackagingDataProvider;
+use Dhl\ShippingCore\Model\ShippingOption\OrderDataProvider;
 use Dhl\ShippingCore\Model\ShippingOption\Selection\OrderSelectionRepository;
 use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Registry;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Sales\Model\Order;
-use Magento\Sales\Model\Order\ShipmentDocumentFactory;
 
 /**
  * ShippingServices
@@ -34,14 +32,9 @@ class ShippingServices implements ArgumentInterface
     private $registry;
 
     /**
-     * @var PackagingDataProvider
+     * @var OrderDataProvider
      */
-    private $packagingDataProvider;
-
-    /**
-     * @var ShipmentDocumentFactory
-     */
-    private $shipmentFactory;
+    private $orderDataProvider;
 
     /**
      * @var SearchCriteriaBuilder
@@ -57,21 +50,18 @@ class ShippingServices implements ArgumentInterface
      * ShippingServices constructor.
      *
      * @param Registry $registry
-     * @param PackagingDataProvider $packagingDataProvider
-     * @param ShipmentDocumentFactory $shipmentFactory
+     * @param OrderDataProvider $orderDataProvider
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
      * @param OrderSelectionRepository $selectionRepository
      */
     public function __construct(
         Registry $registry,
-        PackagingDataProvider $packagingDataProvider,
-        ShipmentDocumentFactory $shipmentFactory,
+        OrderDataProvider $orderDataProvider,
         SearchCriteriaBuilder $searchCriteriaBuilder,
         OrderSelectionRepository $selectionRepository
     ) {
         $this->registry = $registry;
-        $this->packagingDataProvider = $packagingDataProvider;
-        $this->shipmentFactory = $shipmentFactory;
+        $this->orderDataProvider = $orderDataProvider;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
         $this->selectionRepository = $selectionRepository;
     }
@@ -84,10 +74,8 @@ class ShippingServices implements ArgumentInterface
         /** @var Order $order */
         $order = $this->registry->registry('current_order');
         $shippingAddress = $order->getShippingAddress();
-        if (!$shippingAddress) {
-            return [];
-        }
-        if ($shippingAddress && !$shippingAddress->getId()) {
+
+        if (!$shippingAddress || !$shippingAddress->getId()) {
             return [];
         }
         $selections = $this->loadSelections($shippingAddress);
@@ -98,23 +86,12 @@ class ShippingServices implements ArgumentInterface
         }
 
         /** need to create a tmp shipment for packagingDataProvider */
-        try {
-            /** @var Order\Shipment $shipment */
-            $shipment = $this->shipmentFactory->create($order);
-            $packagingData = $this->packagingDataProvider->getData($shipment);
-        } catch (LocalizedException $e) {
+        $carrierData = $this->orderDataProvider->getShippingOptions($order);
+        if ($carrierData === null) {
             return [];
         }
 
-        $carrierCode = strtok((string) $order->getShippingMethod(), '_');
-        $carrierData = $packagingData->getCarriers();
-
-        // no carrier data for given carrier code - so we can exit here.
-        if (!isset($carrierData[$carrierCode])) {
-            return [];
-        }
-
-        $serviceOptions = $carrierData[$carrierCode]->getServiceOptions() ?? [];
+        $serviceOptions = $carrierData->getServiceOptions() ?? [];
         $result = [];
         foreach ($selections as $selection) {
             $shippingOptionCode = $selection->getShippingOptionCode();
