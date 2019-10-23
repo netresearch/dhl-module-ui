14e0c7092c89fda5b15324ba5b7fa57732b7de1a
Andreas Müller <andreas.mueller@netresearch.de>
2019-10-23 15:09:12 +0200

    DHLGW-621: Enable location remove

--- view/frontend/web/js/view/input/shopfinder.js
+++ view/frontend/web/js/view/input/shopfinder.js
@@ -16,7 +16,10 @@ define([
             modalId: "modal-shopfinder",
             modalMapId: "modal-shopfinder-map",
             buttonLabel: '${ $.shippingOptionInput.label }',
-            selectedLocation: null
+            selectedLocation: null,
+            listens: {
+                selectedLocation: "updateSelections"
+            }
         },
 
         initObservable: function () {
@@ -25,58 +28,93 @@ define([
             return this;
         },
 
+        initialize: function () {
+            this._super();
+            this.selectedLocation(this.initSelectedLocation());
+            return this;
+        },
+
+        /**
+         * Try to create a basic initial location object from selection data
+         * or set to null.
+         *
+         * @return {DhlLocation|null}
+         */
+        initSelectedLocation: function () {
+            if (selections.getShippingOptionValue(this.shippingOption.code, this.shippingOptionInput.code)) {
+                return {
+                    'shop_name': selections.getShippingOptionValue(this.shippingOption.code, 'shop-name'),
+                    'address': {
+                        'street': selections.getShippingOptionValue(this.shippingOption.code, 'address-street'),
+                        'postal_code': selections.getShippingOptionValue(this.shippingOption.code, 'address-postalcode'),
+                        'city': selections.getShippingOptionValue(this.shippingOption.code, 'address-city')
+                    }
+                };
+            }
+
+            return null;
+        },
+
         /**
          * Initialize the shopfinder modal als jQuery UI plugin.
          * Method is executed from template on afterRender event
          * to make sure the container is available in the DOM.
          */
         initModal: function () {
-            if (selections.getShippingOptionValue(this.shippingOption.code, this.shippingOptionInput.code)) {
-                this.selectedLocation(
-                    {
-                        'shop_name': selections.getShippingOptionValue(this.shippingOption.code, 'shop-name'),
-                        'address': {
-                            'street': selections.getShippingOptionValue(this.shippingOption.code, 'address-street'),
-                            'postal_code': selections.getShippingOptionValue(this.shippingOption.code, 'address-postalcode'),
-                            'city': selections.getShippingOptionValue(this.shippingOption.code, 'address-city'),
-                        }
-                    }
-                );
-            }
             this.modal = modal(
-                {
-                    trigger: '[data-trigger=showShopfinder]',
-                    responsive: true,
-                    opened: this.onOpen.bind(this),
-                    buttons: []
-                },
+                {responsive: true, buttons: []},
                 $('#' + this.modalId)
             );
         },
 
         /**
+         * Update the selections model when the selectedLocation changes
+         *
+         * @param {DhlLocation|null} location
+         */
+        updateSelections: function (location) {
+            if (location === null || !location.shop_id) {
+                selections.removeSelection(this.shippingOption.code, 'shop-name');
+                selections.removeSelection(this.shippingOption.code, this.shippingOptionInput.code);
+                selections.removeSelection(this.shippingOption.code, 'address-street');
+                selections.removeSelection(this.shippingOption.code, 'address-postalcode');
+                selections.removeSelection(this.shippingOption.code, 'address-city');
+                selections.removeSelection(this.shippingOption.code, 'address-country');
+            } else {
+                selections.addSelection(this.shippingOption.code, 'shop-name', location.shop_name);
+                selections.addSelection(this.shippingOption.code, this.shippingOptionInput.code, location.shop_id);
+                selections.addSelection(this.shippingOption.code, 'address-street', location.address.street);
+                selections.addSelection(this.shippingOption.code, 'address-postalcode', location.address.postal_code);
+                selections.addSelection(this.shippingOption.code, 'address-city', location.address.city);
+                selections.addSelection(this.shippingOption.code, 'address-country', location.address.country);
+            }
+        },
+
+        /**
+         * @public
          * @param {DhlLocation} location
          */
         selectLocation: function (location) {
             this.selectedLocation(location);
-
-            selections.addSelection(this.shippingOption.code, 'shop-name', location.shop_name);
-            selections.addSelection(this.shippingOption.code, this.shippingOptionInput.code, location.shop_id);
-            selections.addSelection(this.shippingOption.code, 'address-street', location.address.street);
-            selections.addSelection(this.shippingOption.code, 'address-postalcode', location.address.postal_code);
-            selections.addSelection(this.shippingOption.code, 'address-city', location.address.city);
-            selections.addSelection(this.shippingOption.code, 'address-country', location.address.country);
-
             if (this.modal) {
                 this.modal.closeModal();
             }
         },
 
         /**
-         * Initialize the map and load locations when opening the modal
+         * Reset selected location
          */
-        onOpen: function () {
+        removeLocation: function () {
+            this.selectedLocation(null);
+        },
+
+        /**
+         * Open the modal, initialize the map and load locations.
+         */
+        openModal: function () {
             var address = quote.shippingAddress();
+
+            this.modal.openModal();
 
             map.init(this.modalMapId, 0.0, 0.0, 13);
 
