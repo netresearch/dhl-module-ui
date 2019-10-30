847da40a7f65a80b79e05c9b7019cd4029136fee
Sebastian Ertner <sebastian.ertner@netresearch.de>
2019-10-30 11:11:10 +0100

    DHLGW-638: fix acces to total segment

--- view/frontend/web/js/view/additional-fee.js
+++ view/frontend/web/js/view/additional-fee.js
@@ -1,60 +1,64 @@
 define([
-        'Magento_Tax/js/view/checkout/cart/totals/shipping',
-        'Magento_Checkout/js/model/totals',
-    ], function (Component, totals) {
-        "use strict";
-        return Component.extend({
-            defaults: {
-                template: 'Dhl_Ui/checkout/summary/shipping'
-            },
-            /**
-             * @return {String}
-             */
-            getIncludingValue: function () {
-                var price;
+    'Magento_Tax/js/view/checkout/cart/totals/shipping',
+    'Magento_Checkout/js/model/totals',
+], function (Component, totals) {
+    "use strict";
+    return Component.extend({
+        defaults: {
+            template: 'Dhl_Ui/checkout/summary/shipping'
+        },
 
-                if (!this.isCalculated()) {
-                    return this.notCalculatedMessage;
-                }
-                price = totals.getSegment('dhlgw_additional_fee').extension_attributes['dhlgw_fee_incl_tax'];
+        /**
+         * @return {String}
+         */
+        getIncludingValue: function () {
+            var price;
 
-                return this.getFormattedPrice(price);
-            },
-
-            /**
-             * @return {String}
-             */
-            getExcludingValue: function () {
-                var price;
-
-                if (!this.isCalculated()) {
-                    return this.notCalculatedMessage;
-                }
-                price = totals.getSegment('dhlgw_additional_fee').extension_attributes['dhlgw_fee'];
-
-                return this.getFormattedPrice(price);
-            },
-
-            /**
-             * @return {Number}
-             */
-            getValue: function () {
-                return totals.getSegment('dhlgw_additional_fee').value;
-            },
-
-
-            /**
-             * @return {string}
-             */
-            getTitle: function () {
-                var title = '';
-
-                if (totals.getSegment('dhlgw_additional_fee')) {
-                    title = totals.getSegment('dhlgw_additional_fee').title;
-                }
-
-                return title;
+            if (!this.isCalculated()) {
+                return this.notCalculatedMessage;
             }
-        });
-    }
-);
+            price = totals.getSegment('dhlgw_additional_fee').extension_attributes['dhlgw_fee_incl_tax'];
+
+            return this.getFormattedPrice(price);
+        },
+
+        /**
+         * @return {String}
+         */
+        getExcludingValue: function () {
+            var price;
+
+            if (!this.isCalculated()) {
+                return this.notCalculatedMessage;
+            }
+            price = totals.getSegment('dhlgw_additional_fee').extension_attributes['dhlgw_fee'];
+
+            return this.getFormattedPrice(price);
+        },
+
+        /**
+         * @return {Number}
+         */
+        getValue: function () {
+            return totals.getSegment('dhlgw_additional_fee')
+                ? totals.getSegment('dhlgw_additional_fee').value
+                : 0;
+        },
+
+
+        /**
+         * @return {string}
+         */
+        getTitle: function () {
+            var title = '';
+
+            if (totals.getSegment('dhlgw_additional_fee')) {
+                title = totals.getSegment('dhlgw_additional_fee')
+                    ? totals.getSegment('dhlgw_additional_fee').title
+                    : '';
+            }
+
+            return title;
+        }
+    });
+});
