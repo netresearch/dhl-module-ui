b49d1bd39adb26ded43efed063f6a04e23311726
Andreas Müller <andreas.mueller@netresearch.de>
2020-01-07 14:40:19 +0000

    DHLGW-731: Quote-specific settings and selections in local storage

--- view/frontend/web/js/model/checkout/storage.js
+++ view/frontend/web/js/model/checkout/storage.js
@@ -1,17 +1,37 @@
 define([
-    'jquery'
-], function ($) {
+    'jquery',
+    'Magento_Checkout/js/model/quote'
+], function ($, quote) {
     'use strict';
 
-    var storage = $.initNamespaceStorage('dhl_shipping_data_storage').localStorage;
+    var storage = $.initNamespaceStorage('dhl_shipping_data_storage').localStorage,
+        quoteId = quote.getQuoteId();
 
     return {
+        /**
+         * @param {string} key
+         * @return {boolean|{}}
+         */
         get: function (key) {
-            return storage.get(key);
-        },
+            var data = storage.get(quoteId);
 
+            if (data !== undefined && data[key] !== undefined) {
+                return data[key];
+            }
+            return false;
+        },
+        /**
+         * @param {string} key
+         * @param {*} value
+         */
         set: function (key, value) {
-            storage.set(key, value);
+            var data = storage.get(quoteId);
+
+            if (data === undefined) {
+                data = {};
+            }
+            data[key] = value;
+            storage.set(quoteId, data);
         },
 
         clear: function () {
